51f279c21ad9892637cbd8204bb17f57a25a32ef
Abhishek Raj Poudel <abhishekrajpoudel@gmail.com>
2025-01-01 22:05:49 +0545

    Made a Laravel project

---
+++ resources/js/Pages/Dashboard.tsx
@@ -0,0 +1,27 @@
+import { Input } from '@/components/ui/input';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Dashboard() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Dashboard
+                </h2>
+            }
+        >
+            <Head title="Dashboard" />
+
+            <div className="py-12">
+                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                    <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <Input type='text' placeholder='Your name' className='lg:w-1/2' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
